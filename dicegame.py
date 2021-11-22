260756b5a723c2062a959e3eaa3ec048aeea6372
viktorpolitehtop <viktor.kovalenko@internet.ru>
2021-11-22 15:12:16 +0600

    2. roll_dice function

--- dicegame.py
+++ dicegame.py
@@ -12,3 +12,17 @@ c = tk.Canvas(r, width=700, height=700)
 c.pack()
 
 i = ''
+
+#Second commit (roll_dice function)
+def roll_dice():
+    global img,i,bttn_clicks
+    dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    d = {'\u2680':1, '\u2681':2, '\u2682':3, '\u2683':4, '\u2684':5, '\u2685':6}
+    die1 = random.choice(dice)
+    die2 = random.choice(dice)
+    ldice.configure(text=f'{die1} {die2}')
+    c.create_window(350, 250, window=ldice)
+    res = d[die1]+d[die2]
+    label2.configure(text="Ты получил  "+str(res))
+    bttn_clicks += 1
+    label1['text'] = "Кости были брошены: " + str(bttn_clicks) + " раз(а)"
